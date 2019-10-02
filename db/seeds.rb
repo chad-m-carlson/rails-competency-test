0dbc030bb604b56ff62317f75036dfc9fea38dce
Chad <chad.m.carlson@gmail.com>
2019-10-01 22:32:54 -0600

    seeds file for users and articles created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def user_creation(role, editor)
+  puts "User Created!"
+  if editor
+    puts "Email: #{role}#{editor}@test.com"
+  else
+    puts "Email: #{role}@test.com"
+  end
+  puts "Password: 'password'"
+  puts "role: #{role}"
+  if editor
+    puts "200 cat related articles psoted for #{role}#{editor}"
+  end
+  puts ""
+end
+
+roles = ["user", "admin", "editor", "editor", "editor"]
+editor_email = 0
+roles.each do |role|
+  if role == "editor"
+    editor_email += 1
+    u = User.create(
+      email: "#{role}#{editor_email}@test.com",
+      password: "password", 
+      roles: role)
+      user_creation(role, editor_email)
+    else
+      u = User.create(
+        email: "#{role}@test.com",
+        password: "password", 
+        roles: role
+        )
+      user_creation(role, nil)
+  end
+  if u.roles[0] == :editor
+    200.times do
+      Article.create(
+        title: Faker::Creature::Cat.name,
+        content: Faker::Lorem.paragraph(sentence_count: rand(10..30)),
+        category: Faker::Creature::Cat.breed,
+        user_id: u.id
+      )
+    end
+  end
+end
+
